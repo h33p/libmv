933531580b4dc4b65601d785cedc16506d615d7b
Sergey Sharybin <sergey.vfx@gmail.com>
2014-03-20 23:07:34 +0600

    Compilation fixes for MinGW
    Many thanks to Antony Riakiotakis for the patch!

--- src/third_party/glog/src/windows/glog/logging.h
+++ src/third_party/glog/src/windows/glog/logging.h
@@ -54,7 +54,7 @@
 
 // Annoying stuff for windows -- makes sure clients can import these functions
 #ifndef GOOGLE_GLOG_DLL_DECL
-# if defined(_WIN32) && !defined(__CYGWIN__)
+# if defined(_WIN32) && !defined(__CYGWIN__) && !defined(__MINGW32__)
 #   define GOOGLE_GLOG_DLL_DECL  __declspec(dllimport)
 # else
 #   define GOOGLE_GLOG_DLL_DECL
@@ -85,7 +85,7 @@
 #include <inttypes.h>           // a third place for uint16_t or u_int16_t
 #endif
 
-#if HAVE_LIB_GFLAGS
+#if 1
 #include <gflags/gflags.h>
 #endif
 
@@ -100,7 +100,7 @@
 
 namespace google {
 
-#if 0      // the C99 format
+#if defined(__MINGW32__)      // the C99 format
 typedef int32_t int32;
 typedef uint32_t uint32;
 typedef int64_t int64;
@@ -110,7 +110,7 @@ typedef int32_t int32;
 typedef u_int32_t uint32;
 typedef int64_t int64;
 typedef u_int64_t uint64;
-#elif 1    // the windows (vc7) format
+#elif defined(_MSC_VER)    // the windows (vc7) format
 typedef __int32 int32;
 typedef unsigned __int32 uint32;
 typedef __int64 int64;
